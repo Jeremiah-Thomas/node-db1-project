d8c0f5b349af9be9e487d8e06ea158bd2b6ff8d9
Jeremiah Thomas <jeremiah.b.thomas12@gmail.com>
2022-08-15 22:53:37 -0400

    All db access functions are working

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,22 +1,33 @@
+const db = require("../../data/db-config");
+
 const getAll = () => {
   // DO YOUR MAGIC
-}
+  return db("accounts");
+};
 
-const getById = id => {
+const getById = (id) => {
   // DO YOUR MAGIC
-}
+  return db("accounts").where("id", id);
+};
 
-const create = account => {
+const create = async (account) => {
   // DO YOUR MAGIC
-}
+  const result = await db("accounts").insert(account);
+  return getById(result);
+};
 
-const updateById = (id, account) => {
+const updateById = async (id, account) => {
   // DO YOUR MAGIC
-}
+  await db("accounts").where("id", id).update(account);
+  return getById(id);
+};
 
-const deleteById = id => {
+const deleteById = async (id) => {
   // DO YOUR MAGIC
-}
+  const account = await getById(id);
+  await db("accounts").where("id", id).delete();
+  return account;
+};
 
 module.exports = {
   getAll,
@@ -24,4 +35,4 @@ module.exports = {
   create,
   updateById,
   deleteById,
-}
+};
--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,27 +1,43 @@
-const router = require('express').Router()
+const router = require("express").Router();
 
-router.get('/', (req, res, next) => {
-  // DO YOUR MAGIC
-})
+const {
+  getAll,
+  getById,
+  create,
+  updateById,
+  deleteById,
+} = require("./accounts-model");
 
-router.get('/:id', (req, res, next) => {
+router.get("/", (req, res, next) => {
   // DO YOUR MAGIC
-})
-
-router.post('/', (req, res, next) => {
-  // DO YOUR MAGIC
-})
-
-router.put('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+  getAll().then((accounts) => {
+    res.json(accounts);
+  });
 });
 
-router.delete('/:id', (req, res, next) => {
+router.get("/:id", (req, res, next) => {
   // DO YOUR MAGIC
-})
+  getById(req.params.id).then((account) => res.json(account));
+});
 
-router.use((err, req, res, next) => { // eslint-disable-line
+router.post("/", (req, res, next) => {
   // DO YOUR MAGIC
-})
+  create(req.body).then((account) => res.json(account));
+});
+
+router.put("/:id", (req, res, next) => {
+  // DO YOUR MAGIC
+  updateById(req.params.id, req.body).then((account) => res.json(account));
+});
+
+router.delete("/:id", (req, res, next) => {
+  // DO YOUR MAGIC
+  deleteById(req.params.id).then((account) => res.json(account));
+});
+
+router.use((err, req, res, next) => {
+  // eslint-disable-line
+  // DO YOUR MAGIC
+});
 
 module.exports = router;
